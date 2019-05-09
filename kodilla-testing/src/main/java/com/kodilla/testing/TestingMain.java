86e77ddf48f80c27d72c158a8fa0ceaed542d11f
RadoslawSulikowski <radoslaw.sulikowski@gmail.com>
2019-05-09 11:48:52 +0200

    CalculatorTest

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        Calculator calculator = new Calculator();
+
+        int result = calculator.add(4, 8);
+
+        if(result == (4 + 8)){
+            System.out.println("test Calculator.add(a, b) method: OK!");
+        } else {
+            System.out.println("test Calculator.add(a, b) method: Something went wrong...");
+        }
+
+        result = calculator.subtract(547,403);
+
+        if(result == (547 - 403)){
+            System.out.println("test Calculator.subtract(minuend, subtrahend) method: OK!");
+        } else {
+            System.out.println("test Calculator.subtract(minuend, subtrahend) method: Something went wrong...");
+        }
     }
 }
