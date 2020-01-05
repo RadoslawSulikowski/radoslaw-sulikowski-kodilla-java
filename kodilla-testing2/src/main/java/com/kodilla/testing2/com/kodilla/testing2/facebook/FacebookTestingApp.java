6cf7418c102b88c41ceeb8c62c1794aa2079be86
RadoslawSulikowski <radoslaw.sulikowski@gmail.com>
2020-01-05 03:28:05 +0100

    Facebook Testing App

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -0,0 +1,27 @@
+package com.kodilla.testing2.com.kodilla.testing2.facebook;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class FacebookTestingApp {
+    public static final String XPATH_DAY_SELECT = "//div[@id=\"birthday_wrapper\"]/div[2]/span/span/select[1]";
+    public static final String XPATH_MONTH_SELECT = "//div[@id=\"birthday_wrapper\"]/div[2]/span/span/select[2]";
+    public static final String XPATH_YEAR_SELECT = "//div[@id=\"birthday_wrapper\"]/div[2]/span/span/select[3]";
+    public static void main(String[] args) {
+
+
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.facebook.com/");
+
+        Select daySelectBox = new Select(driver.findElement(By.xpath(XPATH_DAY_SELECT)));
+        Select monthSelectBox = new Select(driver.findElement(By.xpath(XPATH_MONTH_SELECT)));
+        Select yearSelectBox = new Select(driver.findElement(By.xpath(XPATH_YEAR_SELECT)));
+
+        while(!driver.findElement(By.xpath(XPATH_DAY_SELECT)).isDisplayed());
+        daySelectBox.selectByValue("13");
+        monthSelectBox.selectByVisibleText("cze");
+        yearSelectBox.selectByValue("2000");
+    }
+}
